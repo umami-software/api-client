d4685d0b13bcf0af763ba43ff02d000f123d2afd
Mike Cao <mike@mikecao.com>
2023-04-07 16:02:43 -0700

    Fix regex queries.

--- src/client.ts
+++ src/client.ts
@@ -5,94 +5,94 @@ const API = Symbol();
 
 export const queryMap = [
   {
-    path: /^teams/,
+    path: /^teams$/,
     get: async () => client.getTeams(),
     post: async (args, data) => client.createTeam(data),
   },
   {
-    path: /^teams\/join/,
+    path: /^teams\/join$/,
     post: async (args, data) => client.joinTeam(data),
   },
   {
-    path: /^teams\/[0-9-]+/,
+    path: /^teams\/[0-9-]+$/,
     get: async ([, id]) => client.getTeam(id),
     post: async ([, id], data) => client.updateTeam(id, data),
     delete: async ([, id]) => client.deleteTeam(id),
   },
-  { path: /^teams\/[0-9-]+\/users/, get: async ([, id]) => client.getTeamUsers(id) },
+  { path: /^teams\/[0-9-]+\/users$/, get: async ([, id]) => client.getTeamUsers(id) },
   {
-    path: /^team\/[0-9-]+\/users\/[0-9-]+/,
+    path: /^team\/[0-9-]+\/users\/[0-9-]+$/,
     delete: async ([, teamId, , userId]) => client.deleteTeamUser(teamId, userId),
   },
   {
-    path: /^teams\/[0-9-]+\/websites/,
+    path: /^teams\/[0-9-]+\/websites$/,
     get: async ([, id]) => client.getTeamWebsites(id),
     post: async ([, id], data) => client.createTeamWebsites(id, data),
   },
   {
-    path: /^teams\/[0-9-]+\/websites\/[0-9-]+/,
+    path: /^teams\/[0-9-]+\/websites\/[0-9-]+$/,
     delete: async ([, teamId, , websiteId]) => client.deleteTeamWebsite(teamId, websiteId),
   },
   {
-    path: /^websites/,
+    path: /^websites$/,
     get: async () => client.getWebsites(),
     post: async (args, data) => client.createWebsite(data),
   },
   {
-    path: /^websites\/[0-9-]+/,
+    path: /^websites\/[0-9-]+$/,
     get: async ([, id]) => client.getWebsite(id),
     post: async ([, id], data) => client.updateWebsite(id, data),
     delete: async ([, id]) => client.deleteWebsite(id),
   },
   {
-    path: /^websites\/[0-9-]+\/active/,
+    path: /^websites\/[0-9-]+\/active$/,
     get: async ([, id]) => client.getWebsiteActive(id),
   },
   {
-    path: /^websites\/[0-9-]+\/eventdata/,
+    path: /^websites\/[0-9-]+\/eventdata$/,
     get: async ([, id], data) => client.getWebsiteEventData(id, data),
   },
   {
-    path: /^websites\/[0-9-]+\/events/,
+    path: /^websites\/[0-9-]+\/events$/,
     get: async ([, id], data) => client.getWebsiteEvents(id, data),
   },
   {
-    path: /^websites\/[0-9-]+\/metrics/,
+    path: /^websites\/[0-9-]+\/metrics$/,
     get: async ([, id], data) => client.getWebsiteMetrics(id, data),
   },
   {
-    path: /^websites\/[0-9-]+\/pageviews/,
+    path: /^websites\/[0-9-]+\/pageviews$/,
     get: async ([, id], data) => client.getWebsitePageviews(id, data),
   },
   {
-    path: /^websites\/[0-9-]+\/reset/,
+    path: /^websites\/[0-9-]+\/reset$/,
     post: ([, id]) => client.resetWebsite(id),
   },
   {
-    path: /^websites\/[0-9-]+\/stats/,
+    path: /^websites\/[0-9-]+\/stats$/,
     get: async ([, id], data) => client.getWebsiteStats(id, data),
   },
   {
-    path: /^users/,
+    path: /^users$/,
     get: async () => client.getUsers(),
     post: async (args, data) => client.createUser(data),
   },
   {
-    path: /^users\/[0-9-]+/,
+    path: /^users\/[0-9-]+$/,
     get: async ([, id]) => client.getUser(id),
     post: async ([, id], data) => client.updateUser(id, data),
     delete: async ([, id]) => client.deleteUser(id),
   },
   {
-    path: /^users\/[0-9-]+\/password/,
+    path: /^users\/[0-9-]+\/password$/,
     post: async ([, id], data) => client.updateUserPassword(id, data),
   },
   {
-    path: /^users\/[0-9-]+\/websites/,
+    path: /^users\/[0-9-]+\/websites$/,
     get: async ([, id]) => client.getUserWebsites(id),
   },
   {
-    path: /^users\/[0-9-]+\/teams/,
+    path: /^users\/[0-9-]+\/teams$/,
     get: async ([, id]) => client.getUserTeams(id),
   },
 ];
