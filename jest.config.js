0f7de84ba484ac5a3d77f743880c8774cda86ee7
Paul Popus <paul@nouance.io>
2023-08-11 22:37:20 +0300

    Add new tests in jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   verbose: true,
-  testEnvironment: 'jsdom',
+  /* testEnvironment: 'jsdom', */
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   moduleNameMapper: {
     '\\.(css|less|jpg|png|svg)$': '<rootDir>/test/mocks/emptyModule.js',
--- test.js
+++ test.js
@@ -1,15 +1,34 @@
+require('cross-fetch/polyfill');
 const apiClient = require('./dist/cjs/index');
 const dotenv = require('dotenv');
 dotenv.config();
 
-(async () => {
-  apiClient.client = apiClient.getClient();
+describe('Testing all get functions', () => {
+  beforeAll(() => {
+    apiClient.client = apiClient.getClient();
+    return;
+  });
 
-  try {
-    const x = await apiClient.client.getWebsites();
-
-    console.log(x);
-  } catch (e) {
-    console.log('ERROR', { statusCode: e.statusCode, message: e.message, e });
+  async function testGetWebsites() {
+    const results = await apiClient.client.getWebsites();
+    return results.ok;
   }
-})();
+
+  test('Testing: getWebsites', () => {
+    return expect(testGetWebsites()).resolves.toBeTruthy();
+  });
+
+  async function testGetWebsitesStats() {
+    const results = await apiClient.client.getWebsiteStats(process.env.UMAMI_WEBSITE_ID, {
+      startAt: 1685566800000,
+      endAt: 1686916052440,
+      url: '/',
+    });
+
+    return results.ok;
+  }
+
+  test('Testing: getWebsitesStats', () => {
+    return expect(testGetWebsitesStats()).resolves.toBeTruthy();
+  });
+});
