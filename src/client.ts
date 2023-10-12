e222f93821bf4e5c00aa5900c328460117508392
Mike Cao <mike@mikecao.com>
2023-10-11 21:18:15 -0700

    Updated getClient types.

--- src/client.ts
+++ src/client.ts
@@ -2,14 +2,19 @@ import UmamiApiClient from 'UmamiApiClient';
 
 const API = Symbol();
 
-export function getClient(
-  { userId, secret, apiEndpoint, apiKey } = {
-    userId: process.env.UMAMI_API_USER_ID,
-    secret: process.env.UMAMI_API_CLIENT_SECRET,
-    apiEndpoint: process.env.UMAMI_API_CLIENT_ENDPOINT,
-    apiKey: process.env.UMAMI_API_KEY,
-  },
-): UmamiApiClient {
+export function getClient(params?: {
+  userId?: string;
+  secret?: string;
+  apiEndpoint?: string;
+  apiKey?: string;
+}): UmamiApiClient {
+  const {
+    userId = process.env.UMAMI_API_USER_ID,
+    secret = process.env.UMAMI_API_CLIENT_SECRET,
+    apiEndpoint = process.env.UMAMI_API_CLIENT_ENDPOINT,
+    apiKey = process.env.UMAMI_API_KEY,
+  } = params || {};
+
   const apiClient = new UmamiApiClient({
     userId,
     secret,
